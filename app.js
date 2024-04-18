05cd5fc4d00db4c3a45ad5dfe21a53bce3f418e5
obj <dhqudwo2976@naver.com>
2024-04-18 12:12:45 +0900

    Get Method 전송

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express=require('express');
 var app=express();
 
+//ejs
+app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+
 //bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
-app.set('views', __dirname + '/views');
-app.set('views engine', "ejs");
-
 app.get('/', (req, res)=>{
-    res.render('index.ejs',{'data':1234});
+    res.render('index');
+})
+
+app.get('/result', (req,res)=>{
+    var val1=req.query.val1;
+    var val2=req.query.val2;
+    res.render('result',{'val1':val1,'val2':val2});
 })
 
 app.listen(3000, ()=>{
