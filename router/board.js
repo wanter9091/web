93ae421da68ad4beaebdc2d53c18a676175ead22
obj <dhqudwo2976@naver.com>
2024-06-05 17:52:28 +0900

    crud게시판 2_2(내맘대로 마무리)

--- router/board.js
+++ router/board.js
@@ -12,7 +12,7 @@ router.get('/board/write', function(req,res){
 })
 
 router.get('/board', function(req,res){
-    var sql="select * from board";
+    var sql="select * from blog.board";
     db.query(sql, (err, rows)=>{
         res.render('board',{
             rows: rows
@@ -30,6 +30,53 @@ router.get(`/board/view/:idx`, function(req,res){
     })
 })
 
+router.get(`/board/:type/:idx`, function(req,res){
+    var type=req.params.type;
+    var idx=req.params.idx;
+    
+    if (type=="modify"){
+        res.render('board/auth',{
+            act : "수정",
+            act2 : "modify",
+            idx : idx
+        })
+    }
+    if (type=="delete"){
+        res.render('board/auth',{
+            act : "삭제",
+            act2 : "delete",
+            idx : idx
+        })
+    }
+})
+
+router.post(`/board/:type/:idx`, function(req,res){
+    var type=req.params.type;
+    var idx=req.params.idx;
+    var sql="select * from board where idx=?";
+    
+    db.query(sql,[idx],(err,rows)=>{
+        if(rows[0].password!=req.body.password){
+            res.render('board/error');
+        }
+        else{
+            if (type=="modify"){
+                res.render('board/modify',{
+                    target : rows[0]
+                })
+            }
+            if (type=="delete"){
+                res.render('board/delete',{
+                    act : "삭제",
+                    idx : idx
+                })
+            }
+        }
+        
+    });
+})
+
+
 router.post('/board/write', (req,res)=>{
     var writer=req.body.writer;
     var password=req.body.password;
@@ -39,6 +86,23 @@ router.post('/board/write', (req,res)=>{
     var sql="insert into board(idx, writer, password, title, content, date, del_yn) values(null, ?, ? , ? , ?, now(), 'N')";
     db.query(sql, data);
     res.redirect('/board');
+})
+
+router.post('/board/modify/:idx/ok', function(req,res){
+    var idx=req.params.idx;
+    var sql="update board SET writer=?, password=?, title=?, content=?, date=now() where idx=?";
+    
+    var writer=req.body.writer;
+    var password=req.body.password;
+    var title=req.body.title;
+    var content=req.body.content;
+
+    var data=[writer,password,title,content,idx];
+    console.log(data);
+    db.query(sql,data);
+    console.log(2);
+    res.redirect('/board/view/' + idx);
+    console.log(3);
 })
 
 module.exports=router;
